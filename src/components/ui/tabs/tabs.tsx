e4d6ec08778f512ab5318062c778f41a6f4ea12e
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-02-09 15:23:13 +0200

    Update tabs styles

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -13,7 +13,7 @@ export const TabsList = ({
 }: ComponentPropsWithRef<typeof List>) => (
   <List
     className={cn(
-      'inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground',
+      'inline-flex h-10 items-center justify-center gap-1 rounded-md bg-secondary p-1 text-muted-foreground',
       className
     )}
     {...props}
@@ -26,7 +26,7 @@ export const TabsTrigger = ({
 }: ComponentPropsWithRef<typeof Trigger>) => (
   <Trigger
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+      'inline-flex cursor-default select-none items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all duration-100 state-layer hover:state-layer-foreground/5 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:bg-foreground/10 active:duration-0 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm active:data-[state=active]:bg-neutral-100 dark:active:data-[state=active]:bg-neutral-900',
       className
     )}
     {...props}
