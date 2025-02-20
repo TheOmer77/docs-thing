114bd9b36c0638ab728b7cc2b0957c59937df456
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-02-20 16:33:37 +0200

    Use `bg-mix` classes in command search item

--- src/components/layout/search/search-item.tsx
+++ src/components/layout/search/search-item.tsx
@@ -10,7 +10,7 @@ export type SearchItemProps = ComponentProps<typeof CommandItem> & {
 
 export const SearchItem = ({ doc, parentDoc, ...props }: SearchItemProps) => (
   <CommandItem
-    className='relative flex min-h-12 cursor-default select-none flex-row items-center justify-start rounded-lg px-4 py-2 text-base outline-none transition-[background-color] duration-100 state-layer active:duration-0 aria-selected:state-layer-muted/30 aria-selected:active:bg-muted/30 data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 md:min-h-10 md:text-sm [&>*]:z-10'
+    className='relative flex min-h-12 select-none flex-row items-center justify-start rounded-lg px-4 py-2 text-base outline-none transition-[background-color] duration-100 bg-mix-transparent mix-with-muted-foreground active:duration-0 aria-selected:mix-amount-10 aria-selected:active:mix-amount-20 data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 md:min-h-10 md:text-sm [&>*]:z-10'
     {...props}
   >
     <span className='relative'>
