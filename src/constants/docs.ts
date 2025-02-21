f468f760f1a543e5f2e628f6111e50052e3ceae0
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-02-21 16:15:44 +0200

    Add sorted allDocs constant

--- src/constants/docs.ts
+++ src/constants/docs.ts
@@ -1,7 +1,42 @@
-import { allConfigs, allDocs } from 'content-collections';
-export { allDocs } from 'content-collections';
+import {
+  allConfigs,
+  allDocs as unsortedAllDocs,
+  type Doc,
+} from 'content-collections';
 
-const filteredDocs = allDocs.filter(doc => {
+// Workaround: Content Collections doesn't support singletons
+if (!allConfigs[0]) throw new Error('Site config (config.json) is missing!');
+export const config = allConfigs[0];
+
+const getParentDoc = (doc: Doc) => {
+  if (!doc.title || !doc.includeInSidebar) return;
+  const parentFolder = doc._meta.path.split('/').slice(0, -1).join('/');
+  return unsortedAllDocs.find(
+    d =>
+      d._meta.path === parentFolder &&
+      d._id.startsWith(`${parentFolder}/index.`)
+  );
+};
+
+export const allDocs = unsortedAllDocs.sort((a, b) => {
+  const [aCategoryIdx, bCategoryIdx] = [a, b].map(doc =>
+    Object.keys(config.categories).findIndex(key =>
+      [doc.category, getParentDoc(doc)?.category].includes(key)
+    )
+  ) as [number, number];
+  const [aFilenameNum, bFilenameNum] = [a, b].map(({ _meta }) =>
+    Number(_meta.fileName.split('-')[0])
+  );
+
+  if (aCategoryIdx !== bCategoryIdx)
+    return aCategoryIdx > bCategoryIdx ? 1 : -1;
+  if (aFilenameNum && bFilenameNum && aFilenameNum !== bFilenameNum)
+    return aFilenameNum > bFilenameNum ? 1 : -1;
+  if (a.url !== b.url) return a.url > b.url ? 1 : -1;
+  return 0;
+});
+
+export const filteredDocs = allDocs.filter(doc => {
   if (!doc.title || !doc.includeInSidebar) return false;
   const parentFolder = doc._meta.path.split('/').slice(0, -1).join('/');
   return !allDocs.some(
@@ -36,6 +71,3 @@ export const sidebarDocs = [
     .sort((a, b) => (a.url > b.url ? 1 : a.url < b.url ? -1 : 0)),
 ];
 
-// Workaround: Content Collections doesn't support singletons
-if (!allConfigs[0]) throw new Error('Site config (config.json) is missing!');
-export const config = allConfigs[0];
